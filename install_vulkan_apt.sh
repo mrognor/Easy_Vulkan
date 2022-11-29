c23b8400eb5a2efdba0ff031980a5ec5a32e5631
Timur Ammaev <timurlego1@yandex.ru>
2022-11-29 11:03:34 +0300

    Add script to install vulkan on linux

---
+++ install_vulkan_apt.sh
@@ -0,0 +1,8 @@
+sudo apt install vulkan-tools -y
+sudo apt install libvulkan-dev -y
+sudo apt install vulkan-validationlayers-dev spirv-tools -y
+sudo apt install libglfw3-dev -y
+sudo apt install libglm-dev -y
+sudo apt install libxxf86vm-dev libxi-dev -y
+sudo apt install glslang-tools -y
+echo "You can launch vkcube or vulkaninfo to check if vulkan works"
