95cdcdc5473820248409caf8003d15dd0fe401e0
Timur Ammaev <timurlego1@yandex.ru>
2022-12-02 13:08:27 +0300

    Update install_vulkan_apt

--- install_vulkan_apt.sh
+++ install_vulkan_apt.sh
@@ -1,3 +1,7 @@
+sudo apt update -y
+sudo apt install g++ -y
+sudo apt install make -y
+sudo apt install cmake -y
 sudo apt install vulkan-tools -y
 sudo apt install libvulkan-dev -y
 sudo apt install vulkan-validationlayers-dev spirv-tools -y
