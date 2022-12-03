cf977207996adb6e55ea317917e35bb2bb7d59e8
Timur Ammaev <timurlego1@yandex.ru>
2022-12-03 09:47:42 +0300

    Add install_vulkan_dnf script

---
+++ scripts/install_vulkan_dnf.sh
@@ -0,0 +1,12 @@
+sudo dnf update -y
+sudo dnf install g++ -y
+sudo dnf install make -y
+sudo dnf install cmake -y
+sudo dnf install vulkan-tools -y 
+sudo dnf install vulkan-loader-devel -y 
+sudo dnf install vulkan-loader-devel -y 
+sudo dnf install mesa-vulkan-devel vulkan-validation-layers-devel -y 
+sudo dnf install glfw-devel -y 
+sudo dnf install glm-devel -y 
+sudo dnf install libXi-devel libXxf86vm-devel -y 
+echo "You can launch vkcube or vulkaninfo to check if vulkan works"
