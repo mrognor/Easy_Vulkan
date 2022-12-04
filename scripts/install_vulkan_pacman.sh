973dee5c4e67eea8a4697c8e7358f973942b7f07
mrognor <timurlego1@yandex.ru>
2022-12-04 14:17:30 +0300

    Add install vulkan pacman script

---
+++ scripts/install_vulkan_pacman.sh
@@ -0,0 +1,7 @@
+sudo pacman -S vulkan-devel 
+sudo pacman -S glfw-wayland
+sudo pacman -S glfw-x11
+sudo pacman -S glm
+sudo pacman -S libxi libxxf86vm
+sudo pacman -S glslang
+echo "You can launch vkcube or vulkaninfo to check if vulkan works"
