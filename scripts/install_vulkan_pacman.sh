c5883fd82f49ca4f1967fa99804eb592fa8ba150
mrognor <47296449+mrognor@users.noreply.github.com>
2022-12-04 16:05:16 +0300

    Update install_vulkan_pacman.sh

--- scripts/install_vulkan_pacman.sh
+++ scripts/install_vulkan_pacman.sh
@@ -1,7 +1,11 @@
-sudo pacman -S vulkan-devel 
-sudo pacman -S glfw-wayland
-sudo pacman -S glfw-x11
-sudo pacman -S glm
-sudo pacman -S libxi libxxf86vm
-sudo pacman -S glslang
+sudo pacman -Syu
+sudo pacman -S g++ -y
+sudo pacman -S make -y
+sudo pacman -S cmake -y
+sudo pacman -S vulkan-devel -y
+sudo pacman -S glfw-wayland -y
+sudo pacman -S glfw-x11 -y
+sudo pacman -S glm -y
+sudo pacman -S libxi libxxf86vm -y
+sudo pacman -S glslang -y
 echo "You can launch vkcube or vulkaninfo to check if vulkan works"
